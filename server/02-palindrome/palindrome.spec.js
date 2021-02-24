bb684032939960b12be8bc44b5d0805112938954
Francine M Lucca <Francine.Lucca@ibm.com>
2021-02-24 11:57:03 -0500

    Added guard statement for non-string

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,4 +1,5 @@
 let isPalindrome = phrase => {
+  if(typeof phrase !== 'string') throw new Error('input must be a string');
   if(phrase === '') throw new Error('empty strings are not palindromes');
   if(phrase.trim() === '') return false;
   return phrase === phrase.split('').reverse().join('')
@@ -23,5 +24,7 @@ describe('the palindrome canary spec', () => {
   it('error for empty string', () => {
     expect(() => {isPalindrome('');}).toThrowError('empty strings are not palindromes');
   });
-  it.todo ('error for not a string');
+  it('error for not a string', () => {
+    expect(() => {isPalindrome(121);}).toThrowError('input must be a string');
+  });
 });
